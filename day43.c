baefa4a600d129b31fa8391d132d30fa433b151b
SaadDev <zwixozouine@gmail.com>
2026-02-26 00:08:19 +0000

    return min & max in the tree !

---
+++ day43.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// initai treee in c programming 
+
+typedef struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+}Node;
+
+// create
+Node *cree(int val){
+    Node *newNode = malloc(sizeof(Node));
+    
+    if(newNode == NULL){
+        return 0;
+    }
+    
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    
+    return newNode;
+}
+
+// insert 
+Node *insert(Node *root , int val){
+    if(root ==  NULL){
+        return cree(val);
+    }
+    
+    Node *current = root;
+    Node *parent = NULL;
+    
+    while(current != NULL){
+        parent = current;
+        if(val<current->data){
+            current = current->left;
+        } else if(val > current->data){
+            current = current->right;
+        } else{
+            return root;
+        }
+    }
+    
+    if(val < parent->data){
+        parent->left = cree(val);    
+    } else{
+        parent->right = cree(val);
+    }
+    
+    return root;
+}
+
+// return the min and max from te tree
+
+int min(Node *root){
+    
+    if(root ==  NULL){
+        return -1;
+    }
+    
+    Node *temp = root;
+    
+    while(temp->left!=NULL){
+        temp = temp->left;
+    }
+    return temp->data;
+}
+
+int max(Node *root){
+    if(root ==  NULL){
+        return -1;
+    }
+    
+     Node *temp = root;
+    
+    while(temp->right!=NULL){
+        temp = temp->right;
+    }
+    return temp->data; 
+}
+
+int main(){
+    Node *root = NULL;
+    int i=0, x, val; 
+    
+    printf("Give the tree a size : ");
+    scanf("%d", &x);
+    
+    if(x>=1){
+        while(i<x){
+            printf("%d)==> ", i+1);
+            scanf("%d", &val);
+            root = insert(root, val);
+            i++;
+        }
+    }
+    
+    if(root!= NULL){
+        printf("\n Min = %d ", min(root));
+        printf("\n Max = %d ", max(root));
+    }
+    
+    return 0;
+}
